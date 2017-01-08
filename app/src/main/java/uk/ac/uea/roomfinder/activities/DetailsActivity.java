a3184e73a1d116c42d5d0e3fa6a01c4cf3e782cc
Alex Towli <alextowli@gmail.com>
2017-01-08 23:54:54 +0000

    update location details activity & view

--- app/src/main/java/uk/ac/uea/roomfinder/activities/DetailsActivity.java
+++ app/src/main/java/uk/ac/uea/roomfinder/activities/DetailsActivity.java
@@ -1,7 +1,9 @@
 package uk.ac.uea.roomfinder.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import uk.ac.uea.roomfinder.R;
@@ -14,10 +16,16 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_details);
-        roomName = (TextView)findViewById(R.id.roomName);
+        roomName = (TextView)findViewById(R.id.name_tv);
 
         /* Get intent and use passed data */
         long id = (long)getIntent().getExtras().get("id");
         roomName.setText("Location " + id);
     }
+
+    public void mapActivity(View view) {
+        Intent i = new Intent(this, MapActivity.class);
+        startActivity(i);
+    }
+
 }
