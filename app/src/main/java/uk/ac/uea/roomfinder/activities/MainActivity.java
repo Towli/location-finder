a1942dcdfc6e8e2961648079d0c06e610ffb93a3
williampeterpaul <william.p.paul@gmail.com>
2017-01-13 19:21:01 +0000

    Test for csv parser with Site and Building classes

--- app/src/main/java/uk/ac/uea/roomfinder/activities/MainActivity.java
+++ app/src/main/java/uk/ac/uea/roomfinder/activities/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Arrays;
+
 import uk.ac.uea.roomfinder.R;
+import uk.ac.uea.roomfinder.framework.implementation.AndroidCSVParser;
+import uk.ac.uea.roomfinder.framework.implementation.Site;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_main);
+
+        Site s = new Site();
+        s.setBuildings(new AndroidCSVParser().parse("map_data.csv", this));
+        System.out.println(s.getBuildings().get(0).getName());
     }
 
     public void browseActivity(View view) {
