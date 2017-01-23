1c5360146069a87539a3563ef1f724f82fb6b9f0
Alex Towli <alextowli@hotmail.co.uk>
2017-01-23 14:55:14 +0000

    refactor and delete unneeded files - create new help activity

--- app/src/main/java/uk/ac/uea/roomfinder/activities/MainActivity.java
+++ app/src/main/java/uk/ac/uea/roomfinder/activities/MainActivity.java
@@ -1,5 +1,6 @@
-package uk.ac.uea.roomfinder;
+package uk.ac.uea.roomfinder.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,9 +10,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.ac.uea.framework.implementation.AndroidCSVParser;
+import uk.ac.uea.framework.implementation.AndroidInternalFileIO;
+import uk.ac.uea.framework.implementation.Building;
+import uk.ac.uea.framework.implementation.Point;
+import uk.ac.uea.framework.implementation.Site;
+import uk.ac.uea.roomfinder.R;
+import uk.ac.uea.roomfinder.activities.BrowseActivity;
+import uk.ac.uea.roomfinder.activities.SearchActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Site site;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,26 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        site = new Site();
+        List<List<String>> csv = new AndroidCSVParser().parse("map_data.csv", this);
+        List<Building> buildings = new ArrayList<>();
+
+        for(List<String> line : csv) {
+            if(line.get(0).equals("building")) {
+                buildings.add(new Building(line.get(1), new Point(line.get(2), line.get(3)),
+                        line.get(5), line.get(4)));
+            }
+        }
+
+        site.setBuildings(buildings);
+
+
+        new AndroidInternalFileIO().writeObject(buildings, "test", this);
+
+        List<Building> test = (List<Building>) new AndroidInternalFileIO().readObject("test", this);
+        System.out.println(test.get(0).getName());
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -28,6 +64,18 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void browseActivity(View view) {
+        Intent i = new Intent(this, BrowseActivity.class);
+        i.putExtra("site", site);
+        startActivity(i);
+    }
+
+    public void searchActivity(View view) {
+        Intent i = new Intent(this, SearchActivity.class);
+        i.putExtra("site", site);
+        startActivity(i);
     }
 
     @Override
@@ -69,13 +117,18 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            System.out.println("Home");
+            System.out.println("Nav item pressed: Home");
         } else if (id == R.id.nav_browse) {
-            System.out.println("Browse");
+            Intent i = new Intent(this, BrowseActivity.class);
+            i.putExtra("site", site);
+            startActivity(i);
         } else if (id == R.id.nav_search) {
-            System.out.println("Search");
+            Intent i = new Intent(this, SearchActivity.class);
+            i.putExtra("site", site);
+            startActivity(i);
         } else if (id == R.id.nav_help) {
-            System.out.println("Help");
+            Intent i = new Intent(this, HelpActivity.class);
+            startActivity(i);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
